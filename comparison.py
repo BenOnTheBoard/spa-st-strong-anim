2937bd1b710d26f5e763e9e6929662250c689d3b
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-02-18 20:50:29 +0000

    File for comparing bruteforce and algorithm

---
+++ comparison.py
@@ -0,0 +1,11 @@
+from bruteforce import STSMBruteForce
+from SPAST_Strong_Anim import SPAST_STRONG_ANIM
+
+filename = "examples/sofiat/ex3.txt"
+bruteforcer = STSMBruteForce(filename)
+result = bruteforcer.choose(1)
+print(result)
+
+strong_solver = SPAST_STRONG_ANIM(filename)
+strong_solver.inner_repeat()
+print(strong_solver.G)

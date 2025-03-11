27cfbc10fed40c25946ddbe4bc0383af016bb562
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-03-11 20:14:21 +0000

    Generate instances with solutions only

--- genTestInstances.py
+++ genTestInstances.py
@@ -7,11 +7,12 @@ import numpy as np
 from tqdm import tqdm
 
 from instanceGenerator import SPASTIG
+from bruteforce import STSMBruteForce
 
 ######
 # Panel:
-DENSITY_STEPS = 8
-FILES_PER_DENSITY_PAIR = 100
+DENSITY_STEPS = 6
+FILES_PER_DENSITY_PAIR = 1000
 DIRECTORY = "examples/gen/"
 ######
 
@@ -21,23 +22,30 @@ if os.path.isdir(DIRECTORY):
 os.mkdir(DIRECTORY)
 print("Cleanup done.")
 
-density_levels = np.arange(0, 1, 1 / DENSITY_STEPS)
+step_size = 1 / DENSITY_STEPS
+density_levels = np.arange(0, 1 + step_size / 2, step_size)
 density_pairs = list(product(density_levels, density_levels))
 
 for densities in tqdm(density_pairs):
     students = random.randint(1, 4)
-    projects = random.randint(1, 16)
+    projects = random.randint(1, students)
     lecturers = random.randint(1, projects)
 
     for k in range(FILES_PER_DENSITY_PAIR):
-        S = SPASTIG(
-            students=students,
-            projects=projects,
-            lecturers=lecturers,
-            pref_list_length_lb=0,
-            pref_list_length_ub=projects,
-            student_tie_density=densities[0],
-            lecturer_tie_density=densities[1],
-        )
-        filename = f"{DIRECTORY}{int(densities[0] * DENSITY_STEPS)}_{int(densities[1] * DENSITY_STEPS)}_{k}.txt"
-        S.write_instance_with_ties(filename)
+        instance_ssm_list = []
+        while not instance_ssm_list:
+            S = SPASTIG(
+                students=students,
+                projects=projects,
+                lecturers=lecturers,
+                pref_list_length_lb=0,
+                pref_list_length_ub=projects,
+                student_tie_density=densities[0],
+                lecturer_tie_density=densities[1],
+            )
+            filename = f"{DIRECTORY}{int(densities[0] * DENSITY_STEPS)}_{int(densities[1] * DENSITY_STEPS)}_{k}.txt"
+            S.write_instance_with_ties(filename)
+
+            bruteforcer = STSMBruteForce(filename)
+            bruteforcer.choose()
+            instance_ssm_list = bruteforcer.get_ssm_list()
